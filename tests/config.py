eba43062765b69a94db035cfdd78c71077a08f96
oleksandr.nezhurenko <al.nezhurenko@gmail.com>
2022-09-27 14:26:56 +0300

    ver 1.5

--- tests/config.py
+++ tests/config.py
@@ -1,9 +1,9 @@
 from enum import Enum
 
+SERVICE_URL = "https://my-json-server.typicode.com/typicode/demo/posts"
+
 
 class ErrorMessage(Enum):
     WRONG_STATUS_CODE = "Actual code is Not equal to expected"
     WRONG_MESSAGE_LEN = "Actual message length is Not equal to expected"
 
-
-SERVICE_URL = "https://my-json-server.typicode.com/typicode/demo/posts"
