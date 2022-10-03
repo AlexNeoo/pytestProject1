a88b9a51e5bc778f1266fe3b97e2814d8a1792b3
oleksandr.nezhurenko <al.nezhurenko@gmail.com>
2022-10-03 15:48:45 +0300

    ver 3.7

--- tests/helper_json.py
+++ tests/helper_json.py
@@ -14,7 +14,6 @@ class ValidateJson:
         resp_data = self.data.json()
         assert len(resp_data) == 3, \
             ErrorMessage.WRONG_MESSAGE_LEN.value
-        print(resp_data)
 
     def check_schema(self):
         resp_data = self.data.json()
