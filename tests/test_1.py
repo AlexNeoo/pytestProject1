1360f12986d5ae29b5e097fa1ed85b86453fa9a3
oleksandr.nezhurenko <al.nezhurenko@gmail.com>
2022-10-06 00:09:41 +0300

    ver 4.1

--- tests/test_1.py
+++ tests/test_1.py
@@ -31,7 +31,7 @@ def test_4():
 
 def test_5():
     resp = requests.get(url=SERVICE_URL_W)
-
     ValidateJsonPydantic(resp).check_status_code()
+    print(resp)
     # ValidateJsonPydantic(resp).check_len()
     # ValidateJsonPydantic(resp).check_schema()
